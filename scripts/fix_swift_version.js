697a2fa6c678ae5122d945dc556b383c0d1cd0c0
shehan <shehan.salinda@ncinga.net>
2024-11-20 21:48:41 +0530

    add hooks

--- scripts/fix_swift_version.js
+++ scripts/fix_swift_version.js
@@ -1,14 +1,18 @@
-// fix_swift_version.js
 module.exports = function (ctx) {
+    console.log('Running fix_swift_version.js');
+
     const fs = require('fs');
     const path = require('path');
 
     const projectRoot = ctx.opts.projectRoot;
+    console.log("project path :", projectRoot)
     const platformDir = path.join(projectRoot, 'platforms', 'ios');
     const configPath = path.join(platformDir, 'cordova', 'build.xcconfig');
 
     if (fs.existsSync(configPath)) {
-        fs.appendFileSync(configPath, '\nUSE_SWIFT_LANGUAGE_VERSION=5.0\n');
-        console.log('USE_SWIFT_LANGUAGE_VERSION set to 5.0 in build.xcconfig');
+        fs.appendFileSync(configPath, '\nSWIFT_VERSION=5.0\n');
+        console.log('SWIFT_VERSION set to 5.0 in build.xcconfig');
+    } else {
+        console.log('build.xcconfig not found');
     }
 };
